6669c7fd16a0aa97c9b0e203f9b3cd1846000680
Mateusz <mateuszq28@gmail.com>
2025-01-24 23:53:24 +0100

    przed usunieciem typow

--- skrypt_merge.py
+++ skrypt_merge.py
@@ -14,9 +14,14 @@ sektor2 = gdal.Open(sektor2_path)
 array1 = sektor1.ReadAsArray()
 array2 = sektor2.ReadAsArray()
 
-# Diagnostyka - zakresy danych
-print("Sektor 1 - min:", np.min(array1), "max:", np.max(array1))
-print("Sektor 2 - min:", np.min(array2), "max:", np.max(array2))
+def print_diagnostics(array1, array2):
+    # Diagnostyka - zakresy danych
+    print("Sektor 1 - min:", np.min(array1), "max:", np.max(array1))
+    print("Sektor 2 - min:", np.min(array2), "max:", np.max(array2))
+    min_global = min(np.min(array1), np.min(array2))
+    max_global = max(np.max(array1), np.max(array2))
+    print("Global - min:", min_global, "max:", max_global)
+print_diagnostics(array1, array2)
 
 # Diagnostyka - zakresy danych
 print("Po odrzuceniu -9999:")
@@ -35,10 +40,16 @@ def cut_to_min_size(array1, array2):
 
 array1, array2 = cut_to_min_size(array1, array2)
 
+def crop_margin(array, margin=30):
+    return array[margin:-margin, margin:-margin]
+
+array1 = crop_margin(array1)
+array2 = crop_margin(array2)
+
 # Twarde skalowanie wartości (zakres od 0 do 1000)
 def hard_scale(array, min_value=0, max_value=1000):
     array = np.clip(array, min_value, max_value)
-    scaled_array = ((array - min_value) / (max_value - min_value) * 255).astype(np.uint8)
+    scaled_array = ((array - min_value) / (max_value - min_value) * 255).astype(np.uint16)
     return scaled_array
 
 # array1 = hard_scale(array1)
@@ -54,20 +65,50 @@ array2 = replace_negative_with_min(array2)
 
 # Diagnostyka - zakresy danych
 print("po podmianie min")
-print("Sektor 1 - min:", np.min(array1), "max:", np.max(array1))
-print("Sektor 2 - min:", np.min(array2), "max:", np.max(array2))
+print_diagnostics(array1, array2)
 
 
 def min_max_scale(array, min_value=0, max_value=255):
     array = np.clip(array, min_value, max_value)
-    scaled_array = ((array - min(array.flatten())) / (max(array.flatten()) - min(array.flatten())) * 255).astype(np.uint8)
+    scaled_array = ((array - min(array.flatten())) / (max(array.flatten()) - min(array.flatten())) * 255).astype(np.uint16)
     return scaled_array
 
 array1 = min_max_scale(array1)
 array2 = min_max_scale(array2)
 
+def margins(array1, array2):
+    array1_right_margin_avg = np.mean(array1[:, -30:]).astype(np.int16)
+    array2_left_margin_avg = np.mean(array2[:, :30]).astype(np.int16)
+    print("array1_right_margin_avg:", array1_right_margin_avg)
+    print("array2_left_margin_avg:", array2_left_margin_avg)
+    print(type(array1_right_margin_avg))
+    print(type(array2_left_margin_avg))
+    diff = np.abs(array1_right_margin_avg - array2_left_margin_avg)
+    print("diff:", diff)
+    return array1_right_margin_avg, array2_left_margin_avg, diff
+array1_right_margin_avg, array2_left_margin_avg, diff = margins(array1, array2)
+
+def level_out_arrays(array1, array2, array1_avg, array2_avg):
+    print_diagnostics(array1, array2)
+    diff = abs(array1_avg - array2_avg)
+    if array1_avg > array2_avg:
+        array2 += diff
+    else:
+        array1 += diff
+
+level_out_arrays(array1, array2, array1_right_margin_avg, array2_left_margin_avg)
+margins(array1, array2)
+print_diagnostics(array1, array2)
+
+# array1 = min_max_scale(array1)
+# array2 = min_max_scale(array2)
+
+margins(array1, array2)
+print_diagnostics(array1, array2)
+
 # Łączenie sektorów
 combined_array = np.hstack((array1, array2))
+combined_array = min_max_scale(combined_array)
 
 # Zapis jako PNG
 image = Image.fromarray(combined_array)
